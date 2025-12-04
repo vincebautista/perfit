6c1b21b1804f0da3990adb051f6b743a038e4bd1
Vince Bautista <vincebautista678@mail.com>
2025-12-04 14:34:06 +0800

    walk

--- lib/widgets/walk_animation.dart
+++ lib/widgets/walk_animation.dart
@@ -4,7 +4,7 @@ class WalkAnimation extends StatelessWidget {
   final double width;
   final double height;
 
-  const WalkAnimation({super.key, this.width = 100, this.height = 100});
+  const WalkAnimation({super.key, this.width = 300, this.height = 300});
 
   @override
   Widget build(BuildContext context) {
